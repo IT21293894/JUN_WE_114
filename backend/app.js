6c2ec21d527f9a1f94beae27a44c34cf23443897
IT21302626 <it21302626@my.sliit.lk>
2024-05-07 16:50:27 +0530

    IT21302626 - main modified

--- backend/app.js
+++ backend/app.js
@@ -1,40 +1,23 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
+// mallController.js
 
-import odelRouter from "./routes/odel-routes";
-import coolplanetRouter from "./routes/coolplanet-router";
-import router from "./routes/mallRotes";
-import fashionRouter from "./routes/fashion-routes.js";
-import mallRoutes from './routes/mallRoutes.js';
+import Store from "../model/Store.js";
 
+// Controller function for searching stores
+export const searchStore = async (req, res) => {
+  const query = req.query.query;
+  try {
+    const result = await Store.findOne({
+      name: { $regex: new RegExp(query, "i") },
+    });
+    if (result) {
+      res.json(result);
+    } else {
+      res.status(404).json({ error: "Store not found" });
+    }
+  } catch (error) {
+    console.error("Error searching store:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/api/odel", odelRouter);
-app.use("/api/coolplanet", coolplanetRouter);
-
-// Route for mall search
-app.use("/api/mall", router);
-
-app.use("/api/fashion", fashionRouter);
-// Route for mall search
-app.use('/api/mall', mallRoutes);
-mongoose
-  .connect(
-    "mongodb+srv://admin:1zoLtWfZGywrzzEi@cluster0.dzwv3p2.mongodb.net/Mall?retryWrites=true&w=majority&appName=Cluster0"
-  )
-  .then(() => app.listen(5000))
-  .then(() =>
-    console.log("Connected To Database and Listentng To Localhost 5000")
-  )
-  .catch((err) => console.log(err));
-
-//1zoLtWfZGywrzzEi
-//admin
-=======
-    console.log("Connected To Database and Listening To Localhost 5000")
-  )
-  .catch((err) => console.log(err));
 
